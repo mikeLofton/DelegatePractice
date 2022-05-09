4b9ff2d6fc5f453c46cb655b8cdd08ce97e675af
mikeLofton <michaellofton32@gmail.com>
2022-05-09 16:53:09 -0500

    Practice

---
+++ DelegatePractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegatePractice
+{
+    delegate bool Condition(int value);
+    class Program
+    {
+        int[] testArray;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //public int GetValue(Condition condition)
+        //{
+            
+        //}
+    }
+
+    class Hero
+    {
+        private int _health;
+
+        public void TakeDamage(int amount)
+        {
+            _health -= amount;
+        }
+    }
+}
