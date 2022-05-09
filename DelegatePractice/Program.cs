f47401f6832658da2f04a2a2769de84d4fdd6e85
mikeLofton <michaellofton32@gmail.com>
2022-05-09 17:02:50 -0500

    Hero stuff

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -3,13 +3,15 @@
 namespace DelegatePractice
 {
     delegate bool Condition(int value);
+    delegate void Event(params object[] args);
     class Program
     {
         int[] testArray;
 
         static void Main(string[] args)
         {
-            
+            Hero hero = new Hero();
+            hero.OnTakeDamage += args => Console.WriteLine(args[0]);
         }
 
         //public int GetValue(Condition condition)
@@ -21,10 +23,12 @@ namespace DelegatePractice
     class Hero
     {
         private int _health;
+        public Event OnTakeDamage;
 
         public void TakeDamage(int amount)
         {
             _health -= amount;
+            OnTakeDamage.Invoke(_health, amount);
         }
     }
 }
